cda62e3ed0fd89da0d622a59092e3919550b8154
SoloDeveloping <solodeveloping@proton.me>
2025-08-24 12:09:28 +0200

    displaying worker counts with black, green or red color for better experience

--- theLudovyc/GUI/LabelWorkers.gd
+++ theLudovyc/GUI/LabelWorkers.gd
@@ -4,6 +4,8 @@ extends Label
 	set(value):
 		population_type = value
 
+var default_color: Color
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var current_node = get_tree().current_scene
@@ -12,9 +14,19 @@ func _ready():
 		current_node.get_node("EventBus").connect(
 			"available_workers_updated", _on_EventBus_available_workers_updated_updated
 		)
+		
+	default_color = get_theme_color("font_color")
 
 	pass  # Replace with function body.
 
 
 func _on_EventBus_available_workers_updated_updated(available_workers_amount):
-	text = "(" + Helper.get_string_from_signed_int(available_workers_amount[population_type]) + ")"
+	var pop = available_workers_amount[population_type]
+	text = "(" + Helper.get_string_from_signed_int(pop) + ")"
+	if pop < 0:
+		add_theme_color_override("font_color", Color.RED)
+	elif pop == 0:
+		add_theme_color_override("font_color", default_color)
+	else:
+		add_theme_color_override("font_color", Color.DARK_GREEN)
+		
